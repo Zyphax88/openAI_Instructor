e57b086251c51422c61617a39c7733dd23633f4c
Jared Rice <riceusaf@gmail.com>
2023-01-18 06:16:06 -0600

    add last 8 chats to prompt

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import os
 import openai
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from collections import deque
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 historyChat = []
+recentHistory = []
 tokenPrompt = 0
 tokenCompletion = 0
 tokenTotal = 0
@@ -29,14 +31,24 @@ def index():
             frequency_penalty=0,
             presence_penalty=0.6
         )
+
         historyChat.append(response.choices[0].text)
         tokenPrompt = response.usage.prompt_tokens
         tokenCompletion = response.usage.completion_tokens
         tokenTotal = response.usage.total_tokens
 
         return render_template("index.html", historyChat=historyChat, tokenPrompt=tokenPrompt, tokenCompletion=tokenCompletion, tokenTotal=tokenTotal)
-
     return render_template("index.html", historyChat=historyChat, tokenPrompt=0, tokenCompletion=0, tokenTotal=0)
+
+# Grabs the last 8 chats from history
+# Used to pass to prompt in generate_prompt()
+def manageHistoyChat():
+    deq = deque(maxlen=8)
+    for i in historyChat:
+        deq.append(i)
+    recentHistory = list(deq)
+    return "\n".join(recentHistory)
+
 
 def readStudyGuide():
     # Used for testing API without wasting tokens
@@ -54,7 +66,8 @@ def generate_prompt(studentInput):
     trying to become aircraft mechanics. The AI is professional and will answer the student's 
     questions correctly.  If the student asks a question that does not pertain to aircraft or 
     mechanics please remind them to stay on topic.  Only use the following information given 
-    below when helping the student. \n""" + readStudyGuide() + """\nStudent: I need help understanding aircraft mechanics
-    AI Instructor: I am an AI created by OpenAI. What do you need help with today?
+    below when helping the student. \n""" + readStudyGuide() + """\nStudent: I need help 
+    understanding aircraft mechanics AI Instructor: I am an AI created by OpenAI. What do you 
+    need help with today?""" + manageHistoyChat() + """
     Student: {}
     """.format(studentInput)
